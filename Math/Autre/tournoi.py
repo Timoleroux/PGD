e2235293c7e366e606901990386d9162d0415a0c
Timothée Le Roux <timoleroux2006@gmail.com>
2022-06-30 05:56:20 +0200

    V1

--- Math/Autre/tournoi.py
+++ Math/Autre/tournoi.py
@@ -2,13 +2,15 @@ import random as rd
 
 def generate_players(nbr_players):
     duos = {}
-    for i in range(nbr_players):
+    for i in range(nbr_players-1):
         duos[str("duo" + str(i+1))] = ["player" + str(i+1), "player" + str(i+2)]
+    duos[str("duo" + str(i+2))] = ["player" + str(i+2), "player" + str(1)]
     return duos
 
 def tournoi(nbr_players):
 
     duos = generate_players(nbr_players)
+    print(duos)
 
     for i in range(len(duos)):
 
@@ -16,10 +18,33 @@ def tournoi(nbr_players):
             
             win = rd.randint(0,1)
 
-            if win == 0:
-                duos[str("duo" + str(i+1))].append(duos[])
+            duoA = str("duo" + str(i+1))
+
+            if str("duo" + str(j+1)) != duoA:
+                duoB = str("duo" + str(j+1))
+
+                if win == 0:
+                    duos[duoA].append(1)
+                    print("Le " + duoA + " a gagné le duel contre le " + duoB)
+                else:
+                    duos[duoB].append("1")
+                    print("Le " + duoA + " a perdu le duel contre le " + duoB)
+
+            elif str("duo" + str(j+1)) not in duos:
+                duoB = str("duo" + str(1))
+
+                if win == 0:
+                    duos[duoA].append(1)
+                    print("Le " + duoA + " a gagné le duel contre le " + duoB)
+                else:
+                    duos[duoB].append("1")
+                    print("Le " + duoA + " a perdu le duel contre le " + duoB)
+            
             else:
-                duos[str("duo" + str(i+2))].append("1")
+                duoB = str("duo" + str(1))
+
+
+            
 
 
 tournoi(16)
